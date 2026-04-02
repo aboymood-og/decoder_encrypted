53f11b2b33d79458a6f9cac41e775f66da47fa86
Alonso <a.olmedocosta@gmail.com>
2026-04-02 15:12:59 -0300

    Second version

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ HexDic = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 
 Caracteres_Validos = ["*", "&", "#", "!", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
+Diccionario_ascii = {
+    " ": [32, 20, 40, 100000],
+    "!": [33, 21, 41, 100001],
+    '"': [34, 22, 42, 100010],
+    
+}
+
 #Esta funcion lo que hace es leer el archivo caracter por caracter y lo ordena como una gran lista con listas internas las cuales, en principio y sin limpiar, cada sublista seria igual a un caracter ASCII
 def archiveToList(texto):
     #Lectura de .txt caracter por caracter
@@ -52,7 +59,7 @@ def archiveToList(texto):
             break
     archivo.close()
 
-    #print(list)
+    print(list)
     print(f"[+] Procesando archivo: {texto}")
     return list
 
@@ -79,12 +86,93 @@ def filtadoDeDatos(lista):
 
     return lista
 
+#Este def se encargara de recorrer la lista procesada y crear una nueva lista con los numeros originales transformados a la base requerida.
+def listToBaseRquired(lista_og, base_required):
+    lista_transform = []
+    for i in range(len(lista_og)):
+        if base_required == 2:
+            num = toBinary(lista_og[i])
+        elif base_required == 8:
+            num = toOctal(lista_og[i])
+        elif base_required == 10:
+            num = toDecimal(lista_og[i])
+        elif base_required == 16:
+            num = toHex(lista_og[i])
+        lista_transform.append(num)
+    return lista_transform
         
+
+#ex de num a recibir: num = ['!', '0', '1']
+
+#aca mas de lo mismo pero para octal
+def toBinary(num):
+    pass
+
+#aca mas de lo mismo pero para octal
+def toOctal(num):
+    pass
+
+#En este def aplicare un polinomio caracteristico para pasar de cualquier base disponible a decimal en caso de ser necesario.
+def toDecimal(num):
+    if num[0] == "*":
+        pot = 2
+    elif num[0] == "&":
+        pot = 8
+    elif num[0] == "#":
+        pot = 10
+    elif num[0] == "!":
+        pot = 16
+        #aplicar algun tipo de mapeo de A a F como 10 a 15 y reemplazar cada valor unico.
+    
+    num_transformed = 0
+    for i in range(len(num)-1):
+        aux = int(num[i+1])
+        i *= 1
+        aux2 = aux * pot**(len(num)-2-i)
+        num_transformed += aux2
+
+    return num_transformed
         
+#aca mas de lo mismo pero para hex
+def toHex(num):
+    pass
+
+#def para decodificar to ascii
+
+
+#numero en lista separado tpo [["","",""]] to string como tal, unido, 1 solo
+
+
+
+
+
 
 
 #Programa principal:
+print("--- DECODIFICADOR DE NOTAS ---")
 
-megalista = archiveToList("prueba_1.txt")
-cleanlst = filtadoDeDatos(megalista)
-print(cleanlst)
+base_status = False
+while not base_status:
+    base = int(input("Ingrese la base en la que desea visualizar los datos (2, 8, 10, 16): "))
+    if base == 2 or base == 8 or base == 10 or base == 16:
+        base_status = True
+    else:
+        print("Porfavor, eliga una de las bases disponibles")
+
+
+archivo_procesado = archiveToList("prueba_1 copy.txt")
+archivo_filtrado = filtadoDeDatos(archivo_procesado)
+
+print(f"LISTA DE VALORES EXTRAÍDOS (Base {base}):\n--------------------------------------------------")
+
+archivo_trasnformado = listToBaseRquired(archivo_filtrado, base)
+
+for i in range(len(archivo_trasnformado)):
+    print(f"Valor {i}: {archivo_trasnformado[i]}\t(Original: {archivo_procesado[i]})")
+
+print("--------------------------------------------------")
+
+print("MENSAJE DECODIFICADO: ")
+#FUNCION PARA DECODIFICAR
+
+print("[Proceso finalizado con éxito]")
