c21d76f959a25871d9dc4da5435e8107050e0fac
Alonso <a.olmedocosta@gmail.com>
2026-04-03 15:06:08 -0300

    Agregada y mejorada la funcion toDecimal()

--- SD_lab1_Olmedo_Madariaga.py
+++ SD_lab1_Olmedo_Madariaga.py
@@ -12,13 +12,21 @@ Decimal_base = Valid_characters[0:9]
 
 Hex_prefix = Valid_characters[19]
 Hex_base = Valid_characters[0:15]
+Hex_dic = {
+    "A": 10,
+    "B": 11,
+    "C": 12,
+    "D": 13,
+    "E": 14,
+    "F": 15
+}
 
 Valid_prefix = [Binary_prefix, Octal_prefix, Decimal_prefix, Hex_prefix]
 
 
 def readFileAndIgnoreTrash(path_file):
     
-    print(f"\n[+] Procesando archivo: {path_file}")
+    print(f"\n[+] Procesando archivo: {path_file}\n")
 
     file = open(path_file, mode="r")
 
@@ -49,7 +57,9 @@ def readFileAndIgnoreTrash(path_file):
                     character_status = True
                 else:
                     i += 1
-            clean_file.append(character_clean_file)
+            delimiter = ""
+            join_str = delimiter.join(character_clean_file)
+            clean_file.append(join_str)
 
         elif character_file == "":
             file_is_clean = True
@@ -60,7 +70,56 @@ def readFileAndIgnoreTrash(path_file):
 
     return clean_file
 
+def cleanFileToBaseRequired(clean_file, base_required):
+    file_transformed = []
+    for i in range (len(clean_file)):
+        num_dec = toDecimal(clean_file[i])
+        if base_required == 2:
+            num = decimalToBinary(num_dec)
+        elif base_required == 8:
+            num = decimalToOctal(num_dec)
+        elif base_required == 16:
+            num = decimalToHex(num_dec)
+        else: 
+            num = num_dec
+        file_transformed.append(num)
 
+    return file_transformed
+
+
+def toDecimal(num):
+    if num[0] == Binary_prefix:
+        pot = 2
+    elif num[0] == Octal_prefix:
+        pot = 8
+    elif num[0] == Decimal_prefix:
+        pot = 10
+    elif num[0] == Hex_prefix:
+        pot = 16
+        num = list(num)
+        for i in range(len(num)):
+            if num[i] in Hex_dic:
+                num[i] = str(Hex_dic.get(num[i]))
+        
+    num_decimal = 0
+    for i in range(len(num)-1):
+        aux = int(num[i+1])
+        i *= 1
+        aux2 = aux * pot**(len(num)-2-i)
+        num_decimal += aux2 
+    
+    return num_decimal
+
+def decimalToBinary(num):
+    pass
+
+def decimalToOctal(num):
+    pass
+
+def decimalToHex(num):
+    pass
+        
+    
 
 
 
@@ -68,14 +127,19 @@ def readFileAndIgnoreTrash(path_file):
 print("--- DECODIFICADOR DE MENSAJES ---\n")
 
 #Elegir por teclado la base a la que se quiere transformar el mensaje encripado
-base_status = False
-while not base_status:
-    base = input("Ingrese la base en la que desea visualizar los datos (2, 8, 10, 16):")
-    if base in ("2", "8", "10", "16"):    
-        base_status = True
-        base = int(base)
+base = False
+while not base:
+    base_required = input("Ingrese la base en la que desea visualizar los datos (2, 8, 10, 16):")
+    if base_required in ("2", "8", "10", "16"):    
+        base = True
+        base_required = int(base_required)
     else:
         print("Porfavor, eliga una de las bases disponibles")
 
-#Leer el archivo e ignorar basura
-clean_file = readFileAndIgnoreTrash("prueba_6.txt")
+#Leer el archivo e ignorar basura ; return lista de strings: ['*1001110', '&1112', '!75', '#110', '&143', '*1100001']
+clean_file = readFileAndIgnoreTrash("prueba_6.txt") 
+
+print(f"LISTA DE VALORES EXTRAÍDOS (Base {base}):\n--------------------------------------------------")
+
+#Leer el archivo limpio y tranformar cada valor a su base requerida ; return lista de string transformados: 
+transformed_file = cleanFileToBaseRequired(clean_file, base_required)
